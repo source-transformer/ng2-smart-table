4a13134879b0b81ec0e04be7f4e467951b359171
Tim Cassidy <tcassidy@ea.com>
2020-01-09 09:39:09 -0800

    custom-editors: clean up the code in the custom editors a bit

--- projects/demo/src/app/shared/components/nested-editor/nested-editor.component.ts
+++ projects/demo/src/app/shared/components/nested-editor/nested-editor.component.ts
@@ -1,219 +1,159 @@
-import { Component, ViewChild, ElementRef, OnInit, ChangeDetectorRef } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { DefaultEditor, LocalDataSource } from 'ng2-smart-table';
-import { DataSource } from 'projects/ng2-smart-table/src/lib/lib/data-source/data-source';
-import { Grid } from 'projects/ng2-smart-table/src/lib/lib/grid';
-import { Ng2SmartTableComponent } from 'projects/ng2-smart-table/src/lib/ng2-smart-table.component';
 
-@Component({
-  template: `
+@Component( {
+	template: `
   <div class="table-component" [style.width.px]="tableWidth">
-    <ng2-smart-table #smartTable [settings]="settings" [source]="data"
-      (edit)="tableEdit($event)"></ng2-smart-table>
+    <ng2-smart-table #smartTable [settings]="settings" [source]="data"></ng2-smart-table>
   </div>
 `,
-})
+} )
 
-// (edit)="edit.emit($event)"
-
-/*
-`
-    <input [ngClass]="inputClass"
-            #name
-            class="form-control short-input"
-            [name]="cell.getId()"
-            [disabled]="!cell.isEditable()"
-            [placeholder]="cell.getTitle()"
-            (click)="onClick.emit($event)"
-            (keyup)="updateValue()"
-            (keydown.enter)="onEdited.emit($event)"
-            (keydown.esc)="onStopEditing.emit()"><br>
-
-            <div [hidden]="true" #htmlValue>{{cell.getValue()}}</div>
-  `
-*/
-
-// <div [hidden]="true" #htmlValue>{{getValue()}}</div>
-// <div [hidden]="true" #htmlValue>{{cell.getValue()}}</div>
-// <div [hidden]="true" [innerHTML]="cell.getValue()" #htmlValue></div>
-// <div [hidden]="true" [innerHTML]="{{cell.getValue()}}" #htmlValue></div>
-// {{cell.getValue()}}
-
-// export class NestedEditorComponent extends DefaultEditor implements AfterViewInit {
 export class NestedEditorComponent extends DefaultEditor implements OnInit {
 
-  // @ViewChild('name', { static: false }) name: ElementRef;
-  // @ViewChild('htmlValue', { static: false }) htmlValue: ElementRef;
+	data!: LocalDataSource;
+	settings: any;
 
-
-  // @ViewChild('smartTable', { static: false }) smartTable: ElementRef;
-  // @ViewChild('smartTable', { static: false }) smartTable: Grid;
-  @ViewChild('smartTable', { static: false }) smartTable: Ng2SmartTableComponent;
-
-  data: LocalDataSource;
-  settings: any;
-
-  tableWidth = 400;
+	tableWidth = 400;
 
   constructor(private cd: ChangeDetectorRef) {
     super();
 
     // console.log('NestedEditorComponent.constructor: this.cell.getValue():', this.cell.getValue());
     // console.log('NestedEditorComponent.constructor: cd:',cd);
-    console.log('NestedEditorComponent.constructor: columns:', this.columns);
-
-    // this.data = new LocalDataSource([{id: 'foo'}, {id: 'bar'}]);
+    // console.log('NestedEditorComponent.constructor: columns:', this.columns);
+	  // console.log('NestedEditorComponent.constructor: data:',data);
   }
 
-  // data: Object - row data object
-  // source: DataSource - table data source
-  tableEdit(data: object, source: DataSource) {
-    console.log('NestedEditorComponent.tableEdit: data:', data, 'source:', source);
+	pascalCase( s: string ) {
+		s = s.replace( /(\w)(\w*)/g,
+				function( g0, g1, g2 ) {return g1.toUpperCase() + g2.toLowerCase(); } );
+		return s;
+	}
 
-    console.log('NestedEditorComponent.tableEdit: this.smartTable:', this.smartTable);
+	iterateOverAllRows( tableRows: any ) {
+		let computedTableWidth = 400; // default to something more than zero
+		const defaultHeaderId = 'id';
 
-    // this.smartTable.edit(data);
-    for (const property in this.smartTable) {
-      console.log('NestedEditorComponent.tableEdit: property:', property);
-    }
+		if ( !tableRows ) {
+			throw new Error( 'NestedEditorComponent.iterateOverAllRows: invalid/null table rows handed to nested table component' );
+		} else if ( !Array.isArray( tableRows ) ) {
+			throw new Error( 'NestedEditorComponent.iterateOverAllRows: table rows handed to nested table component is not an array!' );
+		} else { // if ((tableRows) && (Array.isArray(tableRows))) {
+			// console.log( 'iterateOverAllRows: tableRows:', tableRows );
+			// we can probably determine the size of our font dynamically instead of hard-coding it to 0.875
+			// also no idea if 14 or 16 is a better increment for px
+			// const fontSize = 0.875; // font-size: 0.875em;
+			const pxIncrement = 16;
+			// const pxRowWidths = [];
+			let pxMaxRowWidth = 0;
+			// the following loop is being used for two things:
+			// 1. set the "schema" (AKA columns) on our ng2-smart-table
+			// 2. take a guess on how wide to set the div containing the nested table
+			for ( let i = 0; i < tableRows.length; ++i ) {
+				const obj = tableRows[i];
 
-    /*const smartTableObj: any = this.smartTable as Ng2SmartTableComponent;
-    smartTableObj.edit(data);
-    */
-    // this.smartTable.edit.emit(data);
+				// There is a "Update Cancel " string in the first column we need to calculate - so not using starting value of zero
+				let currentRowWidth = pxIncrement * 'Update Cancel '.length;
+				if ( typeof obj === 'string' ) {
+					/*this.settings.columns[defaultHeaderId] = {title:  this.pascalCase( defaultHeaderId ) };
+					currentRowWidth += obj.length * pxIncrement;
+					*/
+					throw new Error( 'NestedEditorComponent.iterateOverAllRows: unable to handle strings as elements of an array' );
+				} else {
+					for ( const prop in obj ) {
+						if ( !Object.prototype.hasOwnProperty.call( obj, prop ) ) {
+							continue;
+						}
+						if ( prop == null ) { // don't know if this is possible
+							continue;
+						}
 
-  }
+						const columnObj: any = {title:  this.pascalCase( prop ) };
+						this.settings.columns[prop] = columnObj;
 
-  pascalCase(s: string) {
-    s = s.replace(/(\w)(\w*)/g,
-        function(g0, g1, g2) {return g1.toUpperCase() + g2.toLowerCase(); });
-    return s;
-  }
+						let columnHeaderWidth = 0;
+						let valueRowWidth = 0;
 
-  iterateOverAllRows(tableRows) {
-    let computedTableWidth = 400; // default to something more than zero
+						columnHeaderWidth = prop.length * pxIncrement;
+						let maxLength = columnHeaderWidth;
 
-    if (!tableRows) {
-      throw new Error('NestedEditorComponent.iterateOverAllRows: invalid/null table rows handed to nested table component');
-    } else if (!Array.isArray(tableRows)) {
-      throw new Error('NestedEditorComponent.iterateOverAllRows: table rows handed to nested table component is not an array!');
-    } else { // if ((tableRows) && (Array.isArray(tableRows))) {
-      // we can probably determine the size of our font dynamically instead of hard-coding it to 0.875
-      // also no idea if 14 or 16 is a better increment for px
-      // const fontSize = 0.875; // font-size: 0.875em;
-      const pxIncrement = 16;
-      // const pxRowWidths = [];
-      let pxMaxRowWidth = 0;
-      // the following loop is being used for two things:
-      // 1. set the "schema" (AKA columns) on our ng2-smart-table
-      // 2. take a guess on how wide to set the div containing the nested table
-      for (let i = 0; i < tableRows.length; ++i) {
-        const obj = tableRows[i];
+						const valueRowObj = obj[prop];
+						if ( valueRowObj != null ) {
+							const valueRowText = valueRowObj.toString();
+							valueRowWidth = valueRowText.length * pxIncrement;
 
-        // There is a "Update Cancel " string in the first column we need to calculate - so not using starting value of zero
-        let currentRowWidth = pxIncrement * 'Update Cancel '.length;
-        for (const prop in obj) {
-            if (!Object.prototype.hasOwnProperty.call(obj, prop)) {
-              continue;
-            }
-            if (prop == null) { // don't know if this is possible
-              continue;
-            }
-            // this.settings.columns[prop] = {title:  this.pascalCase(prop), width: '1000px' };
-            this.settings.columns[prop] = {title:  this.pascalCase(prop) };
+							maxLength = Math.max( columnHeaderWidth, valueRowWidth );
 
-            let columnHeaderWidth = 0;
-            let valueRowWidth = 0;
+							if ( typeof valueRowObj === 'object' ) {
+								columnObj.valuePrepareFunction = function( data: any ) {
+									// return data.name;
+									return JSON.stringify( data );
+								};
+								// TPC: TODO: we don't have a good/general solution for editting objects - so for the time being - don't allow editting (also the stupid smart-table just displays "[object Object]")
+								columnObj.editable = false;
+							}
+						}
+						currentRowWidth += maxLength;
+						// console.log( 'maxLength:', maxLength, 'columnHeaderWidth:', columnHeaderWidth, 'valueRowWidth:', valueRowWidth );
+					}
+				}
 
-            columnHeaderWidth = prop.length * pxIncrement;
-            let maxLength = columnHeaderWidth;
+				if ( currentRowWidth > pxMaxRowWidth ) {
+					pxMaxRowWidth = currentRowWidth;
+				}
+			}
 
-            const valueRowObj = obj[prop];
-            if (valueRowObj != null) {
-              const valueRowText = valueRowObj.toString();
-              valueRowWidth = valueRowText.length * pxIncrement;
+			// console.log( 'NestedEditorComponent.ngOnInit: computedTableWidth:', computedTableWidth, 'pxMaxRowWidth:', pxMaxRowWidth );
+			computedTableWidth = Math.max( computedTableWidth, pxMaxRowWidth );
+		}
 
-              maxLength = Math.max(columnHeaderWidth, valueRowWidth);
-            }
+		// just testing
+		// computedTableWidth = 1000;
 
-            // pxRowWidths.push(maxLength);
-            currentRowWidth += maxLength;
+		this.tableWidth = computedTableWidth;
+	}
 
-            console.log('maxLength:', maxLength, 'columnHeaderWidth:', columnHeaderWidth, 'valueRowWidth:', valueRowWidth);
-        }
+	ngOnInit() {
+		const value = this.getValue();
 
-        if (currentRowWidth > pxMaxRowWidth) {
-          pxMaxRowWidth = currentRowWidth;
-        }
-      }
+		// console.log( 'NestedEditorComponent.ngOnInit: value:', value, 'this.cell:', this.cell );
 
-      console.log('NestedEditorComponent.ngOnInit: computedTableWidth:', computedTableWidth, 'pxMaxRowWidth:', pxMaxRowWidth);
+		if ( !value ) {
+			console.log( 'NestedEditorComponent.ngOnInit: no value - early out' );
+			return;
+		}
+		// this.settings = {columns: {id: {title: 'ID', width: '*'}}, hideHeader: true, hideSubHeader: true};
+		// we can hide the "other top row" using [hideSubHeader: true] - but we'd lose the add button
+		// we can use [mode:'external' ] as a way to trigger custom actions example for edit we should trigger: grid.edit(this.row);
+		// this.settings = {columns: {}, hideHeader: true, mode: 'external' };
+		this.settings = {
+			columns: {},
+			hideHeader: true,
+			attr: {
+					class: 'smarty-table',
+				},
+		};
 
-      computedTableWidth = Math.max(computedTableWidth, pxMaxRowWidth);
-    }
+	 if (this.columns) {
+    	  this.settings.columns = this.columns;
+    	}
 
-    // just testing
-    // computedTableWidth = 1000;
+		// we call this function for two reasons:
+		// 1. set the "schema" (AKA columns) on our ng2-smart-table
+		// 2. take a guess on how wide to set the div containing the nested table
+		this.iterateOverAllRows( value );
 
-    this.tableWidth = computedTableWidth;
-  }
+		this.data = new LocalDataSource( value );
+		// console.log( 'NestedEditorComponent.ngOnInit: (typeof this.data):', typeof( this.data ), 'Array.isArray:', Array.isArray( this.data ) );
+		// console.log( 'NestedEditorComponent.ngOnInit: this.data:', this.data );
+	}
 
-  // ngAfterViewInit() {
-  ngOnInit() {
-
-    console.log('NestedEditorComponent.ngOnInit: this.cell:', this.cell);
-
-    // if (this.cell.newValue !== '') {
-      // this.name.nativeElement.value = this.getValue();
-    // }
-
-    const value = this.getValue();
-    if (!value) {
-      console.log('NestedEditorComponent.ngOnInit: no value - early out');
-      return;
-    }
-    // this.settings = {columns: {id: {title: 'ID', width: '*'}}, hideHeader: true, hideSubHeader: true};
-    // we can hide the "other top row" using [hideSubHeader: true] - but we'd lose the add button
-    // we can use [mode:'external' ] as a way to trigger custom actions example for edit we should trigger: grid.edit(this.row);
-    // this.settings = {columns: {}, hideHeader: true, mode: 'external' };
-    this.settings = {
-      columns: {},
-      hideHeader: true,
-      attr: {
-          class: 'smarty-table',
-        },
-    };
-
-    if (this.columns) {
-      this.settings.columns = this.columns;
-    }
-
-    // we call this function for two reasons:
-    // 1. set the "schema" (AKA columns) on our ng2-smart-table
-    // 2. take a guess on how wide to set the div containing the nested table
-    this.iterateOverAllRows(value);
-
-    // this.data = this.getValue();
-    this.data = new LocalDataSource(value);
-
-    console.log('NestedEditorComponent.ngOnInit: (typeof this.data):', typeof(this.data), 'Array.isArray:', Array.isArray(this.data));
-
-    console.log('NestedEditorComponent.ngOnInit: this.data:', this.data);
-  }
-/*
-  updateValue() {
-    // const value = this.name.nativeElement.value;
-    console.log('NestedEditorComponent.updateValue: this.data:', this.data);
-    // this.cell.newValue = this.data;
-  }
-*/
-
-getValue(): any {
-
-    const cellValue = this.cell.getValue();
-    // console.log('getValue: this.htmlValue.nativeElement:', this.htmlValue.nativeElement);
-    console.log('NestedEditorComponent.getValue: this.cell.getValue():', cellValue);
-    // return this.htmlValue.nativeElement.innerText;
-    return cellValue;
-  }
+	getValue(): any {
+		const cellValue = this.cell.getValue();
+		// console.log('getValue: this.htmlValue.nativeElement:', this.htmlValue.nativeElement);
+		// console.log( 'NestedEditorComponent.getValue: this.cell.getValue():', cellValue );
+		return cellValue;
+	}
 
 }
